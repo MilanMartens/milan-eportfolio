7c013df393d12a36d9f77d1aeab6d98973ff60c6
Milan <r1034512@student.thomasmore.be>
2026-02-21 18:11:33 +0100

    gsap snapping animatie gemaakt.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-import './style.css'
+import "./style.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// animation.
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".doos");
+
+ScrollTrigger.defaults({
+  markers: true,
+});
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontal-slider",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () => "+=" + document.querySelector(".horizontal-slider").offsetWidth,
+  },
+});
